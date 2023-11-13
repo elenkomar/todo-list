4dd84ce0e98db7c04f0516d9b44464dc386b81a3
Elena Komarova <elenkomarova4@gmail.com>
2023-11-13 18:24:59 +0200

    update projects file

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "crispy_bootstrap4",
+    "crispy_bootstrap5",
     "crispy_forms",
     "service",
 ]
