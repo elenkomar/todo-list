81f1239feafaa1044319d681b9a41ea5bc3c26bf
Elena Komarova <elenkomarova4@gmail.com>
2023-11-13 18:27:32 +0200

    update projects file

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY", "SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DJANGO_DEBUG", "") != "False"
 
-ALLOWED_HOSTS = ["127.0.0.1", ""]
+ALLOWED_HOSTS = ["127.0.0.1", "todo-list-x88m.onrender.com"]
 
 INTERNAL_IPS = [
     "127.0.0.1",
